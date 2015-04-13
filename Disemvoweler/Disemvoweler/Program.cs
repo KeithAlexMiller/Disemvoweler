3781bbfd898587a6ce5f46d5d02763c817685bf4
Keith Miller <keithalexmiller@gmail.com>
2015-04-13 17:51:22 -0600

    Disemvowler - Complete
    Needs comments

--- Disemvoweler/Disemvoweler/Program.cs
+++ Disemvoweler/Disemvoweler/Program.cs
@@ -10,18 +10,44 @@ namespace Disemvoweler
     {
         static void Main(string[] args)
         {
+
+            Disemvoweler("Nickleback is my favorite band. Their songwriting can't be beat!");
+            Disemvoweler("How many bears could Bear Grylls grill if Bear Grylls could grill bears?");
+            Disemvoweler("I'm a code ninja, baby. I make the functions and I make the calls.");
+
             // keep the console open
             Console.ReadKey();
         }
         public static string Disemvoweler(string input)
         {
-            
+            string disemvoweled = string.Empty;
+            string vowelsRemoved = string.Empty;
+            string temp = input.ToLower();
+
+            for (int i = 0; i < input.Length; i++)
+			{
+                if ("aeiou".Contains(temp[i]))
+                {
+                    vowelsRemoved += temp[i];
+
+                }
+                else if ("bcdfghjklmnpqrstvwxyz".Contains(temp[i]))
+                {
+                    disemvoweled += temp[i];
+                }
+                else
+                {
+
+                }
+            }
+           
             // Write out the various string results
-            Console.WriteLine("Original: " );
-            Console.WriteLine("Disemvoweled: " );
-            Console.WriteLine("Vowels Removed: " ); 
+            Console.WriteLine("Original: " + input);
+            Console.WriteLine("Disemvoweled: " + disemvoweled);
+            Console.WriteLine("Vowels Removed: " + vowelsRemoved);
+            Console.WriteLine();
             // Return the Disemvoweled string as well for testing
-            return string.Empty;
+            return disemvoweled;
         }
     }
 }
