266af558825af31685c432d1a2c45336d5ec2a06
Keith Miller <keithalexmiller@gmail.com>
2015-04-13 18:08:55 -0600

    Disemvoweler (Submitted)
    comments added

--- Disemvoweler/Disemvoweler/Program.cs
+++ Disemvoweler/Disemvoweler/Program.cs
@@ -11,6 +11,7 @@ namespace Disemvoweler
         static void Main(string[] args)
         {
 
+            //call function with specific phrases
             Disemvoweler("Nickleback is my favorite band. Their songwriting can't be beat!");
             Disemvoweler("How many bears could Bear Grylls grill if Bear Grylls could grill bears?");
             Disemvoweler("I'm a code ninja, baby. I make the functions and I make the calls.");
@@ -20,22 +21,35 @@ namespace Disemvoweler
         }
         public static string Disemvoweler(string input)
         {
+            //create new string for the 'disemvoweled' text
             string disemvoweled = string.Empty;
+
+            //create new string to contain the vowels removed
             string vowelsRemoved = string.Empty;
+
+            //convert string input to lower case
             string temp = input.ToLower();
 
-            for (int i = 0; i < input.Length; i++)
+            //iterate through loop for each character in temp/input
+            for (int i = 0; i < temp.Length; i++)
 			{
+                // check if vowel
                 if ("aeiou".Contains(temp[i]))
                 {
+                    //add to vowel list
                     vowelsRemoved += temp[i];
 
                 }
+                
+                //check if consonant
                 else if ("bcdfghjklmnpqrstvwxyz".Contains(temp[i]))
                 {
+                    //add to disemvoweled list if char is consonant
                     disemvoweled += temp[i];
                 }
-                else
+                
+                //handles spaces and special characters
+                else 
                 {
 
                 }
